52ec7de49fcb3e8aa28eaa3b8b77c0f046c8390e
Adityaraj <adityarajchouhan824@gmail.com>
2026-04-21 23:29:10 +0530

    removed env file

--- src/components/footer.py
+++ src/components/footer.py
@@ -5,8 +5,15 @@ def footer_home():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; item-align:center;">
-            <p style="color:white;"> Created with ❤️ by</p>
-            <b style="font-weight:bold; color:white;">@Adityaraj</b>
+            <p style="color:white;"> Created with ❤️ by <b>@Adityaraj</b></p>
+        </div>
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; item-align:center;">
+            <p>
+            <a href="https://github.com/adityaraj-12" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">GitHub</a> |
+            <a href="https://www.linkedin.com/in/adityaraj-chouhan/" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">LinkedIn</a> |
+            <a href="https://twitter.com/yourusername" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">Twitter</a> |
+            <a href="https://adityarajmacportfolio.vercel.app/" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">Website</a>
+            </p>
         </div>
 
         """, unsafe_allow_html=True)
@@ -15,8 +22,14 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; item-align:center;">
-            <p style="color:black;"> Created with ❤️ by</p>
-            <b style="font-weight:bold; color:black;">@Adityaraj</b>
+            <p style="color:black;"> Created with ❤️ by <b>@Adityaraj</b></p>
         </div>
-
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; item-align:center;">
+            <p>
+            <a href="https://github.com/adityaraj-12" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">GitHub</a> |
+            <a href="https://www.linkedin.com/in/adityaraj-chouhan/" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">LinkedIn</a> |
+            <a href="https://twitter.com/yourusername" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">Twitter</a> |
+            <a href="https://adityarajmacportfolio.vercel.app/" target="_blank" style="color:#4CAF50; text-decoration:none; margin:0 10px;">Website</a>
+            </p>
+        </div>
         """, unsafe_allow_html=True)
